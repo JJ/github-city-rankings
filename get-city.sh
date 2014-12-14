1c4061d8953dbd0083d77aaaa5ff50cbc5ddc6a3
JJ Merelo <jjmerelo@gmail.com>
2014-12-14 09:49:36 +0100

    Error in script

--- get-city.sh
+++ get-city.sh
@@ -4,7 +4,7 @@ if [[ $# -eq 0 ]] ; then
     echo "Default is Granada, but filenames will be all wrong"
     echo "Better use ./get-city-sh <city name>"
 fi
-coffee check-logins.coffee $@
+coffee get-users.coffee $@
 coffee get-details.coffee $@
 coffee format-users.coffee $@
 mv raw/github-users-stats-$1.json ../top-github-users-data/data/
